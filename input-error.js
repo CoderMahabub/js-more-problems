dd5f2628f51ea6b531cb75f28f13f3fee9827108
CoderMahabub <codermahabub@gmail.com>
2021-08-08 14:41:34 +0600

    Error Handling for wrong input

---
+++ input-error.js
@@ -0,0 +1,21 @@
+//Addition of Two numbers using Function
+function add(num1, num2) {
+    const sum = num1 + num2;
+    return sum;
+}
+const firstTotal = add(63, 7);
+// console.log(firstTotal);
+const secondTotal = add(54, 911);
+// console.log(secondTotal);
+
+
+
+//Function for Multiplicaitonn of Two Numbers
+function mul(num1, num2) {
+    const mul = num1 * num2;
+    return mul;
+}
+const firstMul = mul(54, 43);
+console.log(firstMul);
+const secondMul = mul(2, 4);
+console.log(secondMul);
